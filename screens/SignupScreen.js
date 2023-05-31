65e7ae63088bda984f4a1a5e26ec57dfeb0a6694
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-05-31 12:24:07 +0200

    bugg fix

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { createUser } from "../util/auth";
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function singupHandler({ email, password }) {
+    try{
+      setIsAuthenticating(true);
+      await createUser(email, password);
+    }catch(error){
+      console.log(error)
+    }
+    
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating User..." />;
+  }
+
+  return <AuthContent onAuthenticate={singupHandler} />;
 }
 
 export default SignupScreen;
