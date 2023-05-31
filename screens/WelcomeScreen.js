edbdeab7b11f69e7f7144fb953835073d1897d6d
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-05-31 15:20:29 +0200

    protected api

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+
+  //Protected Api with Token
+  useEffect(() => {
+    axios
+      .get(
+        "https://expenses-a652f-default-rtdb.firebaseio.com/messages%20.json?auth=" +
+          token
+      )
+      .then((response) => {
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
